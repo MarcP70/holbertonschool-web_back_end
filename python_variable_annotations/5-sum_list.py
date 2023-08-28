c2ed0b2abae7db5ee95c38377a72cf61977a457e
MarcP70 <marcpourias@yahoo.fr>
2023-08-28 11:25:46 +0200

    add file 5-sum_list.py that calculate the sum of all the numbers in the input list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Task 5:
+Calculate the sum of all the numbers in the input list.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculate the sum of all the numbers in the input list.
+
+    Args:
+        input_list (List[float]): A list of floating-point numbers.
+
+    Returns:
+        float: The sum of all the numbers in the input list.
+    """
+    return sum(input_list)
