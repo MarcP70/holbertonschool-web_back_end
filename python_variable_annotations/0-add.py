1db1476087e6cc1bf5fd042719c3966d7807d263
MarcP70 <marcpourias@yahoo.fr>
2023-08-28 10:53:12 +0200

    add file 0-add.py that return the sum of two float with type-annotated

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 0: Return the sum of two float numbers"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two float numbers.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of `a` and `b` as a float.
+    """
+    return a + b
