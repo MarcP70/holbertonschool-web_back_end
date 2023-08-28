a97e653099b6804079cb0c7561988b99100d8f98
MarcP70 <marcpourias@yahoo.fr>
2023-08-28 11:07:43 +0200

    add file 3-to_str.py that convert a float number to a string representation

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Task 3:
+Convert a float number to a string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float number to a string representation.
+
+    Args:
+        n (float): The float number to be converted.
+
+    Returns:
+        str: The string representation of the input float number.
+    """
+    return str(n)
