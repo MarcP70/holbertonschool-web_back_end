5ad285e29c5b1fa2e2d58e7bf7174270f9f94965
MarcP70 <marcpourias@yahoo.fr>
2023-08-28 11:02:09 +0200

    add file 2-floor.py that returns the largest integer less than or equal to the input number

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Task 2:
+Returns the largest integer less than or equal to the input number.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the largest integer less than or equal to the input number.
+
+    Args:
+        n (float): The input number.
+
+    Returns:
+        int: The largest integer less than or equal to the input number.
+    """
+    return math.floor(n)
