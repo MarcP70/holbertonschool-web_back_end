d5586e6b29d97b0cb766c46df93f14d1e9c99245
MarcP70 <marcpourias@yahoo.fr>
2023-08-29 12:09:46 +0200

    fix file 0-async_generator.py that add type-annotations

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,9 +6,10 @@ at intervals of 1 second.
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     An asynchronous generator that yields random numbers between 0 and 10
     at intervals of 1 second.
