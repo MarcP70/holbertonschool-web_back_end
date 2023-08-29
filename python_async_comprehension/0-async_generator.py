3d821b74d981f61d9168e57fd14acfa8e372aa1a
MarcP70 <marcpourias@yahoo.fr>
2023-08-29 12:03:51 +0200

    add file 0-async_generator.py that yields random numbers between 0 and 10 at intervals of 1 second, 10 times

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Task 0:
+An asynchronous generator that yields random numbers between 0 and 10
+at intervals of 1 second.
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    An asynchronous generator that yields random numbers between 0 and 10
+    at intervals of 1 second.
+
+    Yields:
+        float: A random number between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
