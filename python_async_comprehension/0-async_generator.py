c19c237a4243597a6bdc759b641db682f8991fc4
MarcP70 <marcpourias@yahoo.fr>
2023-08-30 10:00:40 +0200

    fix file 0-async_generator.py that change AsyncGenerator by Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,10 +6,10 @@ at intervals of 1 second.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields random numbers between 0 and 10
     at intervals of 1 second.
