dc41b7e57615e92d56b952e7a52431668db88be9
MarcP70 <marcpourias@yahoo.fr>
2023-09-04 15:37:20 +0200

    fix file 12-log_stats.py that add docstrings

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 """
-
+Task 11: Retrieves statistics about the logs stored in a MongoDB database and
+    prints the results.
 """
 from pymongo import MongoClient
 
 
 def log_stats():
+    """
+    Retrieves statistics about the logs stored in a MongoDB database and prints
+    the results.
+
+    Returns:
+    None
+    """
     # Connect to the MongoDB database
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
@@ -22,14 +30,16 @@ def log_stats():
         method_counts[method] = count
 
     # Get the count of logs with method=GET and path=/status
-    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+    status_check_count = collection.count_documents({"method": "GET",
+                                                    "path": "/status"})
 
     # Print the statistics
     print(f"{total_logs} logs")
     print("Methods:")
     for method, count in method_counts.items():
-        print(f"    method {method}: {count}")
+        print(f"\tmethod {method}: {count}")
     print(f"{status_check_count} status check")
+
 
 if __name__ == "__main__":
     log_stats()
