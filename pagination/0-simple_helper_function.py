a1cd6e878e9d8432061ed8fd44d30b65a5232154
MarcP70 <marcpourias@yahoo.fr>
2023-09-05 12:10:59 +0200

    add file 0-simple_helper_function.py that return a tuple of size two containing a start index and an end index corresponding to the range of indexes

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Task 0.
+"""
+
+def index_range(page, page_size):
+    """
+    Return a tuple of start and end indexes for a given page and page_size.
+
+    Args:
+        page (int): The current page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start and end indexes for the page.
+    """
+    if page <= 0 or page_size <= 0:
+        return (0, 0)
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
