cf599ca6c65c5b46f36cb55e3726ed94c1c4c901
MarcP70 <marcpourias@yahoo.fr>
2023-09-15 13:02:07 +0200

    filx file 5-building.js that add a condition on constructor

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,22 +1,16 @@
 // 5-building.js
 export default class Building {
   constructor(sqft) {
-    // Verify the type of the 'sqft' attribute during object creation
-    if (typeof sqft === 'number') {
-      // Store the 'sqft' attribute in the underscore attribute version
-      this._sqft = sqft;
-    } else {
-      throw TypeError('Sqft must be a number');
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
   }
 
-  // Getter for the 'sqft' attribute
   get sqft() {
     return this._sqft;
-  }
-
-  // Abstract method that must be implemented by subclasses
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
