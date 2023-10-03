de262e3abbf68edf737f4dc6b840b538682be529
MarcP70 <marcpourias@yahoo.fr>
2023-10-03 12:11:58 +0200

    fix 2-then.js that return the new Promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,15 @@
 function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+  return new Promise((resolve, reject) => {
+    promise
+      .then(() => {
+        console.log('Got a response from the API');
+        resolve({ status: 200, body: 'success' });
+      })
+      .catch(() => {
+        console.log('Got a response from the API');
+        reject(new Error());
+      });
+  });
 }
 
 export default handleResponseFromAPI;
