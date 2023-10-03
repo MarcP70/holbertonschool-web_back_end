d647259d2a131a373989e8d16c1c89aee732af86
MarcP70 <marcpourias@yahoo.fr>
2023-10-03 13:46:02 +0200

    add utils.js, 3-main.js and 3-all.js that Handle multiple successful promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,17 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
+
+  Promise.all([photoPromise, userPromise])
+    .then((values) => {
+      const [photo, user] = values;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
