a45beef24c5144c0e9e52d8d6aa52220c37d336c
MarcP70 <marcpourias@yahoo.fr>
2024-06-17 16:15:24 +0200

    Fix filtered_logger.py to add type annotated on get_db function

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -81,7 +81,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Connect to the MySQL database using credentials from environment variables.
 
