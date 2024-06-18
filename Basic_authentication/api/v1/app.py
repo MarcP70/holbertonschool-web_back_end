62acff493f5dce2a11a772d01dfc14cee7212878
MarcP70 <marcpourias@yahoo.fr>
2024-06-18 15:06:43 +0200

    Fix app.py to add docstring for the def before_request

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -25,28 +25,27 @@ if AUTH_TYPE:
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler
-    """
+    """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def unauthorized(error) -> str:
-    """ Forbidden handler
-    """
+    """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
 
 
 # Define the before_request handler
 @app.before_request
 def before_request():
+    """ Performs authentication and authorization checks before each request.
+    """
     if auth is None:
         return
 
