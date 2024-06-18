3594ab3044121a8d4746d490a495d91604af2d66
MarcP70 <marcpourias@yahoo.fr>
2024-06-18 15:57:50 +0200

    Add main_4.py & update api/v1/auth/basic_auth.py for task 9 'Basic - User credentials'

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -38,3 +38,18 @@ class BasicAuth(Auth):
             return decoded_string
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extracts user email and password from a decoded Base64
+            authorization header.
+        """
+        if decoded_base64_authorization_header is None or not isinstance(
+                decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
