94301372ec7c89e98c91bcf01d62129928b1897c
MarcP70 <marcpourias@yahoo.fr>
2024-06-18 15:47:49 +0200

    Add main_3.py & update api/v1/auth/basic_auth.py for task 8 'Basic - Base64 decode'

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Module of basic authentification """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -23,3 +24,17 @@ class BasicAuth(Auth):
         base64_credentials = authorization_header[6:].strip()
 
         return base64_credentials
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decodes a Base64-encoded string and returns it as UTF-8 string """
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
