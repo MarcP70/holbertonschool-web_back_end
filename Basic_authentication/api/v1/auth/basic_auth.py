84e9f29252a878a0051f93c24dcf9a2d2a732332
MarcP70 <marcpourias@yahoo.fr>
2024-06-18 15:36:39 +0200

    Add main_2.py & update api/v1/auth/basic_auth.py for task 7 'Basic - Base64 part'

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,21 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Basic authentification class """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ Extracts and returns the Base64 part of the Authorization
+            header for Basic Authentication.
+        """
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
+            return None
+
+        # Check if the header starts with 'Basic ' (with space at the end)
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        # Extract the Base64 part after 'Basic ' (which is 6 characters long)
+        base64_credentials = authorization_header[6:].strip()
+
+        return base64_credentials
