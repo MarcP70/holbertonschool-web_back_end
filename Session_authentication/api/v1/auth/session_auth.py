5663c3b9b5b13c44b608db6dc07aea69b66c5d1c
MarcP70 <marcpourias@yahoo.fr>
2024-06-19 14:31:28 +0200

    Add main_4.py & update session_auth.py for task 6 'Use Session ID for identifying a User'

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ Module of session authentification """
-from api.v1.auth.auth import Auth
 import uuid
+from typing import TypeVar
+from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -22,3 +24,15 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Return a User instance based on a cookie value """
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return None
+
+        return User.get(user_id)
