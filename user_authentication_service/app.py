ce017d95000237fb5ede86cf22b4910fcdbf84a7
MarcP70 <marcpourias@yahoo.fr>
2024-06-24 12:15:26 +0200

    Update app.py for task 7 to register a new user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,18 +1,40 @@
 #!/usr/bin/env python3
 """Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 # Create the Flask application
 app = Flask(__name__)
-
+AUTH = Auth()
 
 @app.route("/")
 def welcome():
     """ Welcome methode
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def register_user():
+    """ Register methode
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+
+        user = AUTH.register_user(email, password)
+
+        return jsonify({
+            "email": user.email,
+            "message": "user created"
+        }), 200
+
+    except ValueError:
+        return jsonify({
+            "message": "email already registered"
+        }), 400
 
 
 # Run the Flask application if this script is run directly
