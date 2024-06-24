e049455df6f66b2d7dc162fb757f0798adcd058e
MarcP70 <marcpourias@yahoo.fr>
2024-06-24 13:21:52 +0200

    Fix auth.py to change the format of the generated uuid

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -17,7 +17,7 @@ def _hash_password(password: str) -> bytes:
 
 def _generate_uuid() -> str:
     """Generate a string representation of a new UUID."""
-    return uuid.uuid4().hex
+    return str(uuid.uuid4())
 
 
 class Auth:
