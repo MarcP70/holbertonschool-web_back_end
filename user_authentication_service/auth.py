e542cb49af1ab45d6197996a46117b4a56673f22
MarcP70 <marcpourias@yahoo.fr>
2024-06-24 11:44:06 +0200

    Fix auth.py to change the position of the methode hash password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,18 +7,19 @@ from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+    """Hash a password string using bcrypt and return the salted hash."""
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """Hash a password string using bcrypt and return the salted hash."""
-        salt = bcrypt.gensalt()
-        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed
 
     def register_user(self, email: str, password: str) -> User:
         """Register a new user if the email does not exist,
@@ -28,6 +29,6 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
