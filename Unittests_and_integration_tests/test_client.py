a64eb01f6685532bc1c74e071ccf2195353c5407
MarcP70 <marcpourias@yahoo.fr>
2024-07-02 09:10:05 +0200

    Update test_client.py for task 5 to mocking a property

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Use unit tests to ensure it correctly accesses values.
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -37,6 +37,21 @@ class TestGithubOrgClient(unittest.TestCase):
         # Vérifie que le résultat est celui attendu
         self.assertEqual(result, {"key": "value"})
 
+    def test_public_repos_url(self):
+        """
+        Teste que _public_repos_url retourne la bonne URL
+        basée sur la valeur mockée de org.
+        """
+        with patch.object(GithubOrgClient, 'org', new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {
+                "repos_url": "https://api.github.com/orgs/test-org/repos"}
+
+            client = GithubOrgClient("test-org")
+            result = client._public_repos_url
+
+            # Vérifie que le résultat est l'URL attendue
+            self.assertEqual(
+                result, "https://api.github.com/orgs/test-org/repos")
 
 if __name__ == "__main__":
     unittest.main()
