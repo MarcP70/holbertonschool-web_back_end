e78dfca3f49daf6b49b3f46e0a5dbb90fc688859
MarcP70 <marcpourias@yahoo.fr>
2024-07-30 19:09:17 +0200

    Add 4-redis_advanced_op.js for task 4

---
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,29 @@
+import { createClient } from "redis";
+import redis from "redis";
+
+const client = createClient({
+  url: 'redis://localhost:6380'
+});
+
+client
+  .on('error', (err) =>
+    console.log('Redis client not connected to the server:', err)
+  )
+  .on('connect', () =>
+    console.log('Redis client connected to the server')
+  );
+
+client.hset("HolbertonSchools", "Portland", "50", redis.print);
+client.hset("HolbertonSchools", "Seattle", "80", redis.print);
+client.hset("HolbertonSchools", "New York", "20", redis.print);
+client.hset("HolbertonSchools", "Bogota", "20", redis.print);
+client.hset("HolbertonSchools", "Cali", "40", redis.print);
+client.hset("HolbertonSchools", "Paris", "2", redis.print);
+
+client.hgetall("HolbertonSchools", function (err, reply) {
+  if (err) {
+    console.error('Erreur:', err);
+  } else {
+    console.log(reply); // Affiche le hash complet
+  }
+});
