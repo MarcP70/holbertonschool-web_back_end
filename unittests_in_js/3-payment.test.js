f7c1708d24b937bac8788047e75e933199ee7e59
MarcP70 <marcpourias@yahoo.fr>
2024-08-01 14:51:25 +0200

    Fix 3-payment.test.js

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,23 +1,17 @@
-const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
 const sinon = require('sinon');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const expect = require('chai').expect;
 
 describe('sendPaymentRequestToApi', function() {
-  let calculateNumberSpy;
+	it('should use Utils.calculateNumber with the correct arguments', function() {
+	  const spy = sinon.spy(Utils, 'calculateNumber');
 
-  beforeEach(function() {
-    // Create a spy for Utils.calculateNumber
-    calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-  });
+	  sendPaymentRequestToApi(100, 20);
 
-  afterEach(function() {
-    // Restore the original function
-    calculateNumberSpy.restore();
-  });
+	  expect(spy.calledOnce).to.be.true;
+	  expect(spy.calledWithExactly('SUM', 100, 20)).to.be.true;
 
-  it('should call Utils.calculateNumber with "SUM", 100, 20', function() {
-    sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calledOnceWith('SUM', 100, 20)).to.be.true;
+	  spy.restore();
+	});
   });
-});
