b977613cbeff24b1e89b54b37bc870967b2d1717
MarcP70 <marcpourias@yahoo.fr>
2024-08-05 11:58:09 +0200

    Fix exercise.py for task 2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Writing strings to Redis.
 """
 import redis
-import uuid
+from uuid import uuid4
 from typing import Union, Callable, Optional
 from functools import wraps
 
@@ -37,7 +37,7 @@ class Cache:
         Generate a random key, store the input data in Redis using the random
             key and return the key.
         """
-        key = str(uuid.uuid4())
+        key = str(uuid4())
         self._redis.set(key, data)
         return key
 
@@ -47,19 +47,21 @@ class Cache:
         Retrieve the value from Redis using the provided key and optionally
             apply a conversion function.
         """
-        data = self._redis.get(key)
-        if data is None:
+        value = self._redis.get(key)
+        if not value:
             return None
+
         if fn:
-            return fn(data)
-        return data
+            value = fn(value)
+
+        return value
 
     def get_str(self, key: str) -> Optional[str]:
         """
         Retrieve the value from Redis using the provided key and convert it
             to a string.
         """
-        return self.get(key, fn=lambda d: d.decode('utf-8'))
+        return self.get(key, fn=lambda value: value.decode('utf-8'))
 
     def get_int(self, key: str) -> Optional[int]:
         """
