f771b68a1985e92d059432f38ab1c41885f0845e
MarcP70 <marcpourias@yahoo.fr>
2024-08-05 10:55:03 +0200

    Add 1-main.py & update exercice.py for Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Writing strings to Redis.
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -14,9 +14,36 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        The method generate a random key, store the input data in Redis using
-            the random key and return the key.
+        Generate a random key, store the input data in Redis using the random
+            key and return the key.
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> Union[str, bytes, int, float, None]:
+        """
+        Retrieve the value from Redis using the provided key and optionally
+            apply a conversion function.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve the value from Redis using the provided key and convert it
+            to a string.
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve the value from Redis using the provided key and convert it to
+            an integer.
+        """
+        return self.get(key, fn=int)
