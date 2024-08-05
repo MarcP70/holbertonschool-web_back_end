2fa681ab7f2acbf13020bf6054e7b30ba496a27f
MarcP70 <marcpourias@yahoo.fr>
2024-08-05 15:43:54 +0200

    Add 0-uniq_users.sql to create users database

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Create users table with attributes
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255)
+);
